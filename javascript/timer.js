bf3e951050b8cf6a94d27d2931cc4ad226b321c3
Ryan Nguyen <ryanaknguyen@gmail.com>
2025-04-25 00:46:48 -0700

    Added more JavaScript code to keep the date bar information consistent across pages

---
+++ javascript/timer.js
@@ -0,0 +1,18 @@
+const today = new Date()
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+const daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+renderDate(today)
+
+// Renders the current date at the top of the extension
+function renderDate(today) {
+  const dateBar = document.getElementById("timer-date-bar")
+  const dayOfTheWeek = today.getDay()
+  const month = today.getMonth()
+  const day = today.getDate()
+
+  let dayOfTheWeekWord = daysOfTheWeek[dayOfTheWeek]
+  let monthWord = months[month]
+
+  dateBar.innerHTML = `${dayOfTheWeekWord}, ${monthWord} ${day}`
+}
